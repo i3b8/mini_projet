4bc20f9275d7c8aefbc3c9be7d04279dd802eeb1
i3b8 <ilyes.benayed@epfl.ch>
2022-05-14 10:47:00 +0200

    optimisation code_conducteur

--- conducteur.c
+++ conducteur.c
@@ -119,69 +119,31 @@ static THD_FUNCTION(conducteur_thd,arg)
 {
 	(void)arg;
 	chRegSetThreadName(__FUNCTION__);
-	//systime_t time;
 	int32_t instruction_tab[MAX_NB_INSTRUCTION]={0};
 	int num_instruction=-1;
 	static int wayback=0;
 	while(1)
 	{
-		//time = chVTGetSystemTime();
-		//etat=get_etat_marche()+get_instruction_micro();
 		switch(etat)
 		{
-		/*
-			case 0:   // etat initial ou on va faire marcher le moteur
-				if(!motor_already_ordered)
-				{
-					move_forward();
-					//etat=1; // on passe maintenant à l'état en marche
-					set_etat_marche(1);
-
-				}
-				//chThdSleepUntilWindowed(time, time + MS2ST(1000));
-				chThdSleepMilliseconds(100);
-				break;
-			case 1:
-				//if(!motor_already_ordered)
-
-					motor_stop();
-					//etat=0;
-					set_etat_marche(0); // pour dire qu'on s'est arreté
-					//chThdSleepUntilWindowed(time, time + MS2ST(100));
-					chThdSleepMilliseconds(100);
-					break;
-
-				//else break;
-
-		*/
-		case 0: // c'est cette partie d'initialisation on attend le signal de départ
-			//si signal déclaré on passe à l'état 1
-			if(get_instruction_micro()==1)
+		case 0: //Initial Situation : We need an Instruction Start to go to new state
+			if(get_instruction_micro()==1) // a Start Signal has been Detected from the Micro
 			{
 				etat=1;
+				set_instruction_to_do(0);
 				chThdSleepMilliseconds(100);
 				break;
 			}
 			else
 			{
 				motor_stop();
+				set_instruction_to_do(0);
 				chThdSleepMilliseconds(100);
 				break;
 
 			}
-
-			//instruction_left_right=2; // pour right
-			//chThdSleepMilliseconds(100);
-			//break;
-		case 1: //le signal de départ est donnée , on vérifie si on l'espace est libre
-			/*
-			if(!motor_already_ordered)
-			{
-				move_forward();
-			}
-			*/
-
-			if(get_etat_marche()) //l'espace est libre
+		case 1: //After Receiving the Signal Start, We need to verify by IR_Proximity sensor if road is clear or not before moving
+			if(get_etat_marche()) // No obstacles detected we move to the state of Moving
 			{
 				if(!motor_already_ordered)
 				{
@@ -193,28 +155,31 @@ static THD_FUNCTION(conducteur_thd,arg)
 					break;
 
 				}
+				else
+				{
+					set_instruction_to_do(0);
+					chThdSleepMilliseconds(100);
+					break;
+				}
 			}
-			else
+			else //Here the case that the Robot is initially put in front of an Obstacle, we move to the state"Waiting Instruction to turn"
 			{
 				etat=3;
 				set_etat_micro(2);
 				set_instruction_to_do(0);
 				delay(1000000);
-				chThdSleepMilliseconds(100);
+				chThdSleepMilliseconds(4000);
 				break;
 			}
-			//motor_stop();
-			//set_etat_micro(2);
-			//chThdSleepMilliseconds(100);
-		case 2:// le moteur a commencé à avancer c'est au capteur de detecter l'obstacle
+		case 2:// This situation represent the "Moving Situation": the only thing we need to check is if the road is still clear or not
 
 			if(!wayback){
 
 					if(get_etat_marche()==1 )
 					{
 						chThdSleepMilliseconds(100);
+						set_instruction_to_do(0);
 						break;
-
 					}
 					else
 					{
@@ -225,7 +190,7 @@ static THD_FUNCTION(conducteur_thd,arg)
 						etat=3;
 						set_instruction_to_do(0);
 						set_etat_micro(2);
-						chThdSleepMilliseconds(100);
+						chThdSleepMilliseconds(4000);
 						break;
 
 					}
@@ -260,7 +225,7 @@ static THD_FUNCTION(conducteur_thd,arg)
 			}
 
 
-		case 3: //le moteur s'est arreté devant un obstacle <--> c'est au microphone de donner l'ordre
+		case 3: //This situation : We are in front of an Obstacle: We wait for an instruction detected from the Micro (Left Right or Come Back)
 			if(get_instruction_micro()==2)
 			{
 				if(!motor_already_ordered)
@@ -269,13 +234,18 @@ static THD_FUNCTION(conducteur_thd,arg)
 					num_instruction++;
 					instruction_tab[num_instruction]=-2;
 					move_forward();
-
 					etat=2;
 					set_etat_micro(1);
 					set_instruction_to_do(0);
 					chThdSleepMilliseconds(100);
 					break;
 
+				}
+				else
+				{
+					set_instruction_to_do(0);
+					chThdSleepMilliseconds(100);
+					break;
 				}
 			}
 			else if(get_instruction_micro()==3)
@@ -292,6 +262,13 @@ static THD_FUNCTION(conducteur_thd,arg)
 					chThdSleepMilliseconds(100);
 					break;
 				}
+				else
+				{
+					set_instruction_to_do(0);
+					chThdSleepMilliseconds(100);
+					break;
+
+				}
 			}
 			else if(get_instruction_micro()==11)
 			{
@@ -303,15 +280,19 @@ static THD_FUNCTION(conducteur_thd,arg)
 					motor_stop();
 					etat=2;
 					set_etat_micro(1);
+					set_instruction_to_do(0);
 					wayback=1;
 					chThdSleepMilliseconds(100);
 					break;
 				}
+				else
+				{
+					chThdSleepMilliseconds(100);
+					break;
+
+				}
 			}
 
-
-			chThdSleepMilliseconds(100);
-			break;
 		}
 	}
 
